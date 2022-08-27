7d9c724ef25b3af841c54f51e79c4ac3cffe0396
Gennadiy <renardes@gmail.com>
2022-08-27 14:40:40 +0500

    не выполнять код верхнего уровня при импорте

--- class1.py
+++ class1.py
@@ -5,14 +5,15 @@ class Test:
     def attr3():
         print('C3')
 
-print(f'{Test = }\n{type(Test) = }\n')
-print(f'{Test.__mro__}\n')
-print(dir(Test), end='\n\n')
 
-for attr in dir(Test):
-    if not attr.startswith('__'):
-        obj = getattr(Test, attr)
-        print(f'\t{attr}: {type(obj)}, {obj}\n')
+if __name__ == '__main__':
+    print(f'{Test = }\n{type(Test) = }\n')
+    print(f'{Test.__mro__}\n')
+    print(dir(Test), end='\n\n')
 
-Test.attr3()
+    for attr in dir(Test):
+        if not attr.startswith('__'):
+            obj = getattr(Test, attr)
+            print(f'\t{attr}: {type(obj)}, {obj}')
 
+    Test.attr3()
