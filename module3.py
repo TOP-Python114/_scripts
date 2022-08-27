f7cb1928e5d3764d313efee5ba230142c19594ef
Gennadiy <renardes@gmail.com>
2022-08-27 13:25:16 +0500

    закольцованный импорт

---
+++ module3.py
@@ -0,0 +1,13 @@
+print(f'начало выполнения {__name__}\n')
+
+import module4
+
+M3 = 300
+print(f"{M3 = }")
+
+M3 = 325
+print(f"{M3 = }")
+
+module4.get_M3()
+
+print(f'конец выполнения {__name__}\n')
---
+++ module4.py
@@ -0,0 +1,10 @@
+print(f'начало выполнения {__name__}\n')
+
+import module3
+
+M4 = 400
+
+def get_M3():
+    print(f"{module3.M3 = }")
+
+print(f'конец выполнения {__name__}\n')
