466cadbea5241e982d54dcbf62b9fcc85f1f1cf0
Gennadiy <renardes@gmail.com>
2022-09-03 13:03:53 +0500

    принцип инверсии зависимостей

--- oop_DIP.py
+++ oop_DIP.py
@@ -20,42 +20,48 @@ class Person:
 # интерфейс — прокладка между кодом низкого и верхнего уровней
 class RelationshipsBrowser:
     @abstractmethod
-    def find_all_children(self, name):
+    def find_all_children(self, name: str):
         pass
 
 
+# переменная для аннотации типа
+RelationType = list[tuple[Person, Relations, Person]]
+
 class Relationships(RelationshipsBrowser):
-    """Хранит информацию об отношениях между людьми"""
+    """Хранит информацию об отношениях между людьми."""
     def __init__(self):
-        """Хранилище"""
-        self.relations = []
-    
+        """Хранилище."""
+        self.relations: RelationType = []
+
     def add_relation(self, parent: Person, child: Person):
-        self.relations += [(parent, Relations.PARENT, child),
-                           (child, Relations.CHILD, parent)]
-    
-    def find_all_children(self, name):
-        """Низкоуровневая реализация метода для интерфейса, основанная на реализации хранилища"""
+        self.relations += [
+            (parent, Relations.PARENT, child),
+            (child, Relations.CHILD, parent)
+        ]
+
+    def find_all_children(self, name: str):
+        """Низкоуровневая реализация метода для интерфейса, основанная на реализации хранилища."""
         for rel in self.relations:
             if rel[0].name == name and rel[1] == Relations.PARENT:
                 yield rel[2]
 
 
 class Research:
-    """Класс верхнего уровня для работы с информацией об отношениях людей"""
-    def __init__(self, relations_base):
+    """Класс верхнего уровня для работы с информацией об отношениях людей."""
+    def __init__(self, relations_base: Relationships):
         self.base = relations_base
     
     # нарушение DIP — этот метод через индексацию в данном случае зависит 
     #   от того, как именно реализовано хранилище (списком кортежей) в классе 
     #   низкого уровня
     # def find_all_children(self, name):
-        # for rel in self.relations:
-            # if rel[0].name == name and rel[1] == Relations.PARENT:
-                # yield rel[2]
+    #     for rel in self.base.relations:
+    #         if rel[0].name == name and rel[1] == Relations.PARENT:
+    #             yield rel[2]
     
     # метод, независимый от способа хранения информации
-    def find_all_children(self, name):
+    @staticmethod
+    def find_all_children(name: str):
         return base.find_all_children(name)
 
 
@@ -66,8 +72,3 @@ base.add_relation(Person('Kate'), Person('Max'))
 
 r = Research(base)
 print(*r.find_all_children('John'), sep='\n')
-
-
-
-
-
