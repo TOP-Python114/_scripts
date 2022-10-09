98f546c172e32e4fde6f2e048d2beee1ad8519b5
Gennadiy <renardes@gmail.com>
2022-10-09 14:16:31 +0500

    шаблон Посетитель - реализация с декорированием методов

---
+++ patterns/Поведенческие/Посетитель/visitor2.py
@@ -0,0 +1,72 @@
+from abc import ABC
+from typing import Callable, Type
+
+
+class Expression(ABC):
+    """Описывает объекты математических выражений."""
+    pass
+
+
+class FloatExpression(Expression):
+    def __init__(self, value: float):
+        self.value = value
+
+
+class AdditionExpression(Expression):
+    def __init__(self, left: Expression, right: Expression):
+        self.left = left
+        self.right = right
+
+
+_methods = {}
+
+def visitor(arg_type: Type[Expression]) -> Callable:
+    """Параметризует декоратор."""
+
+    def decorator(method_object: Callable) -> Callable:
+        """Декоратор для метода."""
+        _methods[arg_type.__name__] = method_object
+
+        def _wrapper(instance_object, object_to_process: Expression) -> None:
+            key = object_to_process.__class__.__name__
+            method = _methods[key]
+            return method(instance_object, object_to_process)
+
+        return _wrapper
+
+    return decorator
+
+
+
+class ExpressionPrinter:
+    """Посетитель объектов выражения, накапливающий в буфере строковое представление объектов выражения."""
+    def __init__(self):
+        self.buffer = []
+
+    def __str__(self):
+        return ''.join(self.buffer)
+
+    @visitor(FloatExpression)
+    def visit(self, fe: FloatExpression):
+        self.buffer.append(str(fe.value))
+
+    @visitor(AdditionExpression)
+    def visit(self, ae: AdditionExpression):
+        self.buffer.append('(')
+        self.visit(ae.left)
+        self.buffer.append(' + ')
+        self.visit(ae.right)
+        self.buffer.append(')')
+
+
+# 1 + (2 + 3)
+expr1 = AdditionExpression(
+    FloatExpression(1),
+    AdditionExpression(
+        FloatExpression(2),
+        FloatExpression(3)
+    )
+)
+printer = ExpressionPrinter()
+printer.visit(expr1)
+print(printer)
