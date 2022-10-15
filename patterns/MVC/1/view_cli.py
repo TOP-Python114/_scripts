bb944d5fe9ad3b73ab9233f40a128312d92f4708
Gennadiy <renardes@gmail.com>
2022-10-15 13:52:20 +0500

    компонент Представление — CL интерфейс

---
+++ patterns/MVC/1/view_cli.py
@@ -0,0 +1,32 @@
+"""Компонент Представление (MVC) — интерфейс командной строки."""
+
+from time import sleep
+from typing import Collection
+
+import re
+
+
+def start_view():
+    print('Приветствую!')
+    sleep(2)
+
+
+def ask_if_show_all() -> str:
+    prompt = 'Хотите вывести всю информацию из БД [д/н]?\n'
+    while True:
+        inp = input(prompt)
+        if re.match(r'^[днyn]$', inp):
+            break
+    return inp
+
+
+def show_all_view(data: Collection):
+    print(f'В БД всего {len(data)} элементов')
+    for elem in data:
+        print(elem)
+    sleep(10)
+
+
+def end_view():
+    print('Пока!')
+    sleep(2)
