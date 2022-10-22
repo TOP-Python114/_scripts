c537885c19b91b5497103a5b15495ccbede9cd25
Gennadiy <renardes@gmail.com>
2022-10-22 13:22:31 +0500

    сапёр: Cell

--- patterns/MVC/2/main.py
+++ patterns/MVC/2/main.py
@@ -1,3 +1,25 @@
 """Игра Сапёр — демонстратор использования архитектурного подхода MVC с GUI."""
 
 
+class Cell:
+    """Описывает сущность одной клетки игрового поля."""
+    def __init__(self, row: int, column: int):
+        self.row = row
+        self.column = column
+        self.state: str = 'closed'
+        self.mined: bool = False
+        self.counter: int = 0
+
+    mark_sequence = ('closed', 'flagged', 'questioned')
+    def next_mark(self) -> None:
+        """Циклически переключает состояния клетки."""
+        if self.state in self.mark_sequence:
+            i = self.mark_sequence.index(self.state)
+            self.state = self.mark_sequence[(i+1) % len(self.mark_sequence)]
+
+    def open(self) -> None:
+        """Переключает клетку в состояние 'открыто'."""
+        if self.state != 'flagged':
+            self.state = 'opened'
+
+
