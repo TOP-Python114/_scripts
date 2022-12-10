db63914d6e72e6ba189ed34b36721027a00521c2
Gennadiy <renardes@gmail.com>
2022-12-10 12:32:52 +0500

    переименование пакета ядра проекта

--- django/helloworld/core/urls.py
+++ django/helloworld/core/urls.py
@@ -1,4 +1,4 @@
-"""helloworld URL Configuration
+"""Helloworld core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
