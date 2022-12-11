2ef6f9580a7379b7424d6c39fbf135decf12e505
Gennadiy <renardes@gmail.com>
2022-12-11 13:03:54 +0500

    создание ORM модели, применение миграций

--- django/helloworld/main/models.py
+++ django/helloworld/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models import base
+from django.db.models import fields
 
-# Create your models here.
+
+class Author(base.Model):
+    first_name = fields.CharField(max_length=30)
+    last_name = fields.CharField(max_length=50)
+
+
+class Book(base.Model):
+    title = fields.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Publisher(base.Model):
+    name = fields.CharField(max_length=50)
+    authors = models.ManyToManyField(Author)
+    books = models.ManyToManyField(Book)
+
+
+# после каждого изменения модели
+# py manage.py makemigrations main
+
+# (опционально) компиляция SQL кода в файл
+# py manage.py sqlmigrate %имя_приложения% %номер_миграции% > %имя_приложения%\migrations\%номер_миграции%.sql
+
+# применить миграции
+# py manage.py migrate
