7a1c95063f9f8e0526d0aea3f5d52892726a5d56
Gennadiy <renardes@gmail.com>
2022-12-11 14:00:25 +0500

    добавление записей в БД

--- django/helloworld/main/models.py
+++ django/helloworld/main/models.py
@@ -7,16 +7,23 @@ class Author(base.Model):
     first_name = fields.CharField(max_length=30)
     last_name = fields.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Book(base.Model):
     title = fields.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.title}'
 
 class Publisher(base.Model):
     name = fields.CharField(max_length=50)
     authors = models.ManyToManyField(Author)
     books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 # после каждого изменения модели
